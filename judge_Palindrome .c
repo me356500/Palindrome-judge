f6fdebb22ca22273f793ee5d571935ba615910e4
me356500 <43289907+me356500@users.noreply.github.com>
2018-10-28 17:40:37 +0800

    Update judge_Palindrome .c

--- judge_Palindrome .c
+++ judge_Palindrome .c
@@ -4,6 +4,12 @@
 typedef struct{
     char in[20];
 }str;
+int judge(char a[],int len){
+    for(int i = 0,k = len-1;i<len;i++,k--)
+        if(a[i] != a[k])
+            return 0;
+    return 1;
+}
 void compare(char a[],char f[],char b[]){
     if(strlen(f) > strlen(b))
         strcpy(a,f);
@@ -15,28 +21,16 @@ void compare(char a[],char f[],char b[]){
         strcpy(a,f);
     return;
 }
-void adjust(char a[], int len ){
+void adjust(char a[]){
     char forward[20],backward[20];
-    int times = 0;
-    for(int i = 0,k = len-1;i<len;i++,k--)
-        if(a[i] == a[k])
-            times++;
-    if(times == len)
-        return;
     strcpy(backward,a),strcpy(forward,a);
-    while(times != strlen(backward)){
-        times = 0,backward[strlen(backward)-1] = '\0';
-        for(int i = 0,k = strlen(backward)-1;i < strlen(backward);i++,k--)
-            if(backward[i] == backward[k])
-                times++;
+    while(!judge(backward,strlen(backward))){
+        backward[strlen(backward)-1] = '\0';
     }
-    while(times != strlen(forward)) {
-        times = 0,forward[strlen(forward)-1] = '\0';
-        for(int i = strlen(forward) - 1,k = len - 1;i>-1;i--,k--)
+    while(!judge(forward,strlen(forward))) {
+        forward[strlen(forward)-1] = '\0';
+        for(int i = strlen(forward) - 1,k = strlen(a) - 1;i>-1;i--,k--)
             forward[i] = a[k];
-        for(int i = 0,k = strlen(forward)-1;i<strlen(forward);i++,k--)
-            if(forward[i] == forward[k])
-                times++;
     }
     compare(a,forward,backward);
 }
@@ -48,10 +42,9 @@ int main(void){
         scanf("%s",s[i].in);
         for(int j = 0;j < strlen(s[i].in);j++)
             s[i].in[j] = toupper(s[i].in[j]);
-        adjust(s[i].in,strlen(s[i].in));
+        adjust(s[i].in);
     }
-    for(int i = 0;i <totol_num;i++) {
+    for(int i = 0;i < totol_num;i++) {
         printf("%s\n",s[i].in);
     }
-
 }
